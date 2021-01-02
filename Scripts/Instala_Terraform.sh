9f523f7575099df81065c883d0a006a4d488ce33
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2021-01-02 16:15:20 -0300

    Melhoria no Script do Terraform

--- Scripts/Instala_Terraform.sh
+++ Scripts/Instala_Terraform.sh
@@ -1,5 +1,7 @@
 ## INTALANDO TERRAFORM
+_LAST_UPDATE=`wget -qO- https://releases.hashicorp.com/terraform/ | grep terraform | sed -e 's/<[^>]*>//g;s/^ //g' | grep -v beta | grep -v alpha[0-9] | grep -v rc | grep -v oc | awk -F _  '{print$2}' | head -n 1`
+
 cd /tmp/
-wget https://releases.hashicorp.com/terraform/0.13.2/terraform_0.13.2_linux_amd64.zip
-unzip terraform_0.13.2_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/$_LAST_UPDATE/terraform_$_LAST_UPDATE_linux_amd64.zip
+unzip terraform_$_LAST_UPDATE_linux_amd64.zip
 sudo mv terraform /usr/bin/
